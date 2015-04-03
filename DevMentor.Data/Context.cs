1a8f17e6b066e1a82506ade2bca6fb771fdbe413
pmizel <email@pmizel.de>
2015-04-03 17:24:53 +0200

    add data proj

---
+++ DevMentor.Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using DevMentor.Context;
+using DevMentor.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMentor.Data
+{
+    public partial class Context : DevMentor.Context.FileContext,IObjectContextAdapter
+    {
+        public Context()
+            : base("name=ContentItemContext")
+        {
+        }
+
+        public FileSet<User> Users { get; set; }
+
+        public System.Data.Entity.Core.Objects.ObjectContext ObjectContext
+        {
+            get { return null; }
+        }
+    }
+}
