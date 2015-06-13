c9b0bc26c0b88e6173f9913c00271e4dd84cb3f5
pmizel <email@pmizel.de>
2015-06-13 12:34:38 +0200

    update DemoConsole/Program.cs

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -16,20 +16,53 @@ namespace DemoConsole
             var unit = new UnitOfWork(new Context()); //new Context(new InMemoryStoreStrategy())
             //INSERT
             Console.WriteLine("INSERT PAUL");
-            unit.UserRepository.Insert(new User() { UserName="pmizel",FirstName="Paul", LastName="Mizel"});
+            unit.UserRepository.Insert(new User()
+            {
+                UserName = "pmizel",
+                FirstName = "Paul",
+                LastName = "Mizel",
+                Birthday = new DateTime(1980, 4, 1)
+            });
             Console.WriteLine("INSERT FABIAN");
-            unit.UserRepository.Insert(new User() { UserName = "fraetz", FirstName = "Fabian", LastName = "Raetz" });
+            unit.UserRepository.Insert(new User()
+            {
+                UserName = "fraetz",
+                FirstName = "Fabian",
+                LastName = "Raetz",
+                Birthday = new DateTime(1989, 4, 1)
+            });
             unit.Save();
 
             Console.WriteLine("GET ALL");
-            var users=unit.UserRepository.Get(); //GET ALL
+            var users = unit.UserRepository.Get(); //GET ALL
             foreach (var user in users)
             {
                 Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
             }
 
-            Console.WriteLine("GET PAUL");
-            users = unit.UserRepository.Get(f=>f.FirstName=="Paul"); //GET ALL
+            Console.WriteLine("GET Youngest");
+            var youngest = unit.UserRepository.Get(orderBy: o => o.OrderByDescending(i => i.Birthday)).First();
+            Console.WriteLine("{0} {1} ({2}) - {3:dd.MM.yyyy}", youngest.FirstName,
+                                                                youngest.LastName,
+                                                                youngest.UserName,
+                                                                youngest.Birthday);
+
+            Console.WriteLine("GET PAULs");
+            users = unit.UserRepository.Get(f => f.FirstName == "Paul"); //GET ALL
+            foreach (var user in users)
+            {
+                Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
+            }
+            Console.WriteLine("UPDATE PAULs to PABLOs");
+            foreach (var user in users)
+            {
+                user.FirstName = "Pablo";
+                unit.UserRepository.Update(user);
+            }
+            unit.Save();
+
+            Console.WriteLine("GET ALL");
+            users = unit.UserRepository.Get(); //GET ALL
             foreach (var user in users)
             {
                 Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
--- README.md
+++ README.md
@@ -33,25 +33,48 @@ var unit = new UnitOfWork(new Context()); //new Context(new InMemoryStoreStrateg
 Console.WriteLine("INSERT PAUL");
 unit.UserRepository.Insert(new User() { UserName="pmizel",
                                         FirstName="Paul", 
-										LastName="Mizel"});
+                                        LastName="Mizel",
+                                        Birthday=new DateTime(1980,4,1)});
 Console.WriteLine("INSERT FABIAN");
 unit.UserRepository.Insert(new User() { UserName = "fraetz", 
-                                        FirstName = "Fabian", 
-										LastName = "Raetz" });
+                                        FirstName = "Fabian",
+                                        LastName = "Raetz",
+                                        Birthday = new DateTime(1989, 4, 1)});
 unit.Save();
 
 Console.WriteLine("GET ALL");
 var users=unit.UserRepository.Get(); //GET ALL
 foreach (var user in users)
 {
-	Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
+    Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
 }
 
-Console.WriteLine("GET PAUL");
+Console.WriteLine("GET Youngest");
+var youngest = unit.UserRepository.Get(orderBy: o => o.OrderByDescending(i => i.Birthday)).First(); 
+Console.WriteLine("{0} {1} ({2}) - {3:dd.MM.yyyy}", youngest.FirstName, 
+                                                    youngest.LastName, 
+                                                    youngest.UserName,
+                                                    youngest.Birthday);
+
+Console.WriteLine("GET PAULs");
 users = unit.UserRepository.Get(f=>f.FirstName=="Paul"); //GET ALL
 foreach (var user in users)
 {
-	Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
+    Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
+}
+Console.WriteLine("UPDATE PAULs to PABLOs");
+foreach (var user in users)
+{
+    user.FirstName = "Pablo";
+    unit.UserRepository.Update(user);
+}
+unit.Save();
+
+Console.WriteLine("GET ALL");
+users = unit.UserRepository.Get(); //GET ALL
+foreach (var user in users)
+{
+    Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
 }
 
 //DELETE ALL
@@ -60,11 +83,9 @@ unit.UserRepository.Delete(unit.UserRepository.Get());
 users = unit.UserRepository.Get();
 foreach (var user in users)
 {
-	Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
+    Console.WriteLine("{0} {1} ({2})", user.FirstName, user.LastName, user.UserName);
 }
 unit.Save();
-
-Console.ReadKey();
 ```
 
 ###Output:
